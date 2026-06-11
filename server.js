45bba449c030172529720e4506d9dd36f8b04510
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-06-11 19:43:16 +0600

    added

--- server.js
+++ server.js
@@ -3,9 +3,26 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require('mongoose');
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Portfolio Server Running ...");
