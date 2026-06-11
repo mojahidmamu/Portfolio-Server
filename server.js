cc672e47ce8c756aff18cadd3d64b9a5538ee00a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-06-11 19:53:24 +0600

    added

--- server.js
+++ server.js
@@ -2,34 +2,73 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const mongoose = require('mongoose');
+const nodemailer = require("nodemailer");
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
 
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-async function run() {
-  try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await mongoose.disconnect();
-  }
-}
-run().catch(console.dir);
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+});
 
+//  Message Schema: 
+const messageSchema = new mongoose.Schema(
+{
+    name: String,
+    email: String,
+    subject: String,
+    message: String,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  }
+);
+
+const Message = mongoose.model(
+  "Message",
+  messageSchema
+);
+
+// Nodemailer Transporter
+const transporter =
+  nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.APP_PASSWORD,
+    },
+});
+
+
+// Home route: 
 app.get("/", (req, res) => {
   res.send("Portfolio Server Running ...");
 });
 
+// Contact Form Submit: 
+
+
+// Verify Admin: 
+
+// Get All Messages: 
+
+// Delete Message: 
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server Running on ${PORT}`);
+  console.log(`✅ Server Running on ${PORT}`);
 });
